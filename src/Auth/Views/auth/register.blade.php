301848b7b4037d0a3ae41dc8fe0aa37d9ecb4e6d
rogue707Dev <rogue901126@protonmail.com>
2019-07-16 10:59:11 +0200

    update register user

--- src/Auth/Views/auth/register.blade.php
+++ src/Auth/Views/auth/register.blade.php
@@ -1,6 +1,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
+            @if(session('message'))
+                <div>
+                        <span >{{session('message')}}</span>
+                </div>
+            @endif
+
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
